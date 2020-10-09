eed5f5d534dabe8d8fc11934af501654e2618dbd
Mahdi Khardani <mahdi.kardani@telematicus.com>
2020-10-09 16:47:00 +0100

    Used Google Free Translation Api in Console App.

---
+++ ClassLibraryProject/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryProject
+{
+    public class Class1
+    {
+        public static int Increment(int value)
+        {
+            return value + 1;
+        }
+    }
+}
